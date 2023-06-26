9c101ad4864a8dbc439d5ec8c129d5a7a36337dc
jihuazhong <jihuazhong1@huawei.com>
2023-06-26 15:00:09 +0800

    fix:解决模型权重保存时的报错
    v4.30.2版本适配代码在跑text-classification任务整网用例时在模型权重保存阶段报错，简要报错信息如下：AttributeError:'NpuStorage'
    object hash no attribute 'data_ptr'
    这个修改由pr
    https://github.com/huggingface/transformers/pull/23871
    引入，当前torch_npu未完全实现storage的特性，预计在2.1版本支持，因此在暂时回退社区的修改

--- optimum/ascend/__init__.py
+++ optimum/ascend/__init__.py
@@ -1,7 +1,9 @@
 from .training_args import NPUTrainingArguments
 from .trainer import NPUTrainer
 from .trainer_utils import patch_set_seed
+from .modeling_utils import patch_shard_checkpoint
 
 
 # Monkey patch
 patch_set_seed()
+patch_shard_checkpoint()
---
+++ optimum/ascend/modeling_utils.py
@@ -0,0 +1,77 @@
+# Copyright 2023 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from typing import Dict, Union
+
+import torch
+
+import transformers
+from transformers.utils import WEIGHTS_NAME
+from transformers.utils.hub import convert_file_size_to_int
+from transformers.modeling_utils import dtype_byte_size
+
+
+def shard_checkpoint(
+    state_dict: Dict[str, torch.Tensor], max_shard_size: Union[int, str] = "10GB", weights_name: str = WEIGHTS_NAME
+):
+    """
+    Roll back the modication of https://github.com/huggingface/transformers/pull/23871.
+    The above PR depends on the data_ptr method of the tensor storage object, which is not supported by `torch_npu` < 2.1
+    """
+    max_shard_size = convert_file_size_to_int(max_shard_size)
+
+    sharded_state_dicts = []
+    current_block = {}
+    current_block_size = 0
+    total_size = 0
+
+    for key, weight in state_dict.items():
+        weight_size = weight.numel() * dtype_byte_size(weight.dtype)
+
+        # If this weight is going to tip up over the maximal size, we split.
+        if current_block_size + weight_size > max_shard_size:
+            sharded_state_dicts.append(current_block)
+            current_block = {}
+            current_block_size = 0
+
+        current_block[key] = weight
+        current_block_size += weight_size
+        total_size += weight_size
+
+    # Add the last block
+    sharded_state_dicts.append(current_block)
+
+    # If we only have one shard, we return it
+    if len(sharded_state_dicts) == 1:
+        return {weights_name: sharded_state_dicts[0]}, None
+
+    # Otherwise, let's build the index
+    weight_map = {}
+    shards = {}
+    for idx, shard in enumerate(sharded_state_dicts):
+        shard_file = weights_name.replace(".bin", f"-{idx+1:05d}-of-{len(sharded_state_dicts):05d}.bin")
+        shard_file = shard_file.replace(
+            ".safetensors", f"-{idx + 1:05d}-of-{len(sharded_state_dicts):05d}.safetensors"
+        )
+        shards[shard_file] = shard
+        for key in shard.keys():
+            weight_map[key] = shard_file
+    # Add the metadata
+    metadata = {"total_size": total_size}
+    index = {"metadata": metadata, "weight_map": weight_map}
+    return shards, index
+
+
+def patch_shard_checkpoint():
+    transformers.modeling_utils.shard_checkpoint = shard_checkpoint
