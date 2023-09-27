1016d82fb41ad1cb4f47ffec9ccb6dd84b651c1f
汤云翔 <546783735@qq.com>
2023-09-27 05:38:29 +0000

    !86 【Transformers】【Speech-Pretraining】下游任务启动方式由accelarate统一改为python3 Merge pull request !86 from 汤云翔/master

--- examples/speech-pretraining/scripts/run_wav2vec2.sh
+++ examples/speech-pretraining/scripts/run_wav2vec2.sh
@@ -20,30 +20,29 @@ fi
 
 # 启动训练脚本
 start_time=$(date +%s)
-nohup accelerate launch -m torch.distributed.run --nproc_per_node 8 run_wav2vec2_pretraining_no_trainer.py \
-  --dataset_name="librispeech_asr" \
-	--dataset_config_names clean clean \
-	--dataset_split_names validation test \
-	--model_name_or_path="patrickvonplaten/wav2vec2-base-v2" \
-	--max_train_steps="20000" \
-	--num_warmup_steps="32000" \
-	--gradient_accumulation_steps="8" \
-	--learning_rate="0.005" \
-	--weight_decay="0.01" \
-	--max_duration_in_seconds="20.0" \
-	--min_duration_in_seconds="2.0" \
-	--logging_steps="1" \
-	--saving_steps="10000" \
-	--per_device_train_batch_size="8" \
-	--per_device_eval_batch_size="8" \
-	--adam_beta1="0.9" \
-	--adam_beta2="0.98" \
-	--adam_epsilon="1e-06" \
-	--gradient_checkpointing \
-	--mask_time_prob="0.65" \
-	--mask_time_length="10" \
-  --overwrite_output_dir \
-  --output_dir ./output > ${scripts_path_dir}/output/run_wav2vec2.log 2>&1 &
+nohup python3 -m torch.distributed.run --nproc_per_node 8 run_wav2vec2_pretraining_no_trainer.py \
+    --dataset_name="librispeech_asr" \
+    --dataset_config_names clean clean \
+    --dataset_split_names validation test \
+    --model_name_or_path="patrickvonplaten/wav2vec2-base-v2" \
+    --max_train_steps="20000" \
+    --num_warmup_steps="32000" \
+    --gradient_accumulation_steps="8" \
+    --learning_rate="0.005" \
+    --weight_decay="0.01" \
+    --max_duration_in_seconds="20.0" \
+    --min_duration_in_seconds="2.0" \
+    --logging_steps="1" \
+    --saving_steps="10000" \
+    --per_device_train_batch_size="8" \
+    --per_device_eval_batch_size="8" \
+    --adam_beta1="0.9" \
+    --adam_beta2="0.98" \
+    --adam_epsilon="1e-06" \
+    --gradient_checkpointing \
+    --mask_time_prob="0.65" \
+    --mask_time_length="10" \
+    --output_dir ./output > ${scripts_path_dir}/output/run_wav2vec2.log 2>&1 &
 wait
 end_time=$(date +%s)
 e2e_time=$(( $end_time - $start_time ))
